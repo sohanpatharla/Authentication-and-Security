3e0ef63d7a38d61279126d09a70b5d2833e47bac
sohanpatharla <sohanapatharla@outlook.in>
2023-08-12 11:26:58 +0530

    Body for implementing passport

--- app.js
+++ app.js
@@ -36,32 +36,11 @@ app.get("/login",function (req,res) {
  });
 
  app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser=new User({
-            email:req.body.username,
-            password:hash
-        });
-        newUser.save().then(()=>{
-            res.render("secrets");
-        });
-    });
     
  });
 
  app.post("/login",function (req,res) {
-    const username=req.body.username
-    const password=req.body.password
-    User.findOne({email:username}).then((foundUser)=>{
-        if(foundUser)
-        {
-            bcrypt.compare(password, foundUser.password , function(err, result) {
-                if(result=== true)
-            {
-                res.render("secrets");
-            }
-            });
-        }
-    })  
+
  });
 
 app.listen(3000, function() {
